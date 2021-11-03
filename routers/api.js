b04b8176bd58a7bbe8b75b7745323d4c2f248a06
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-03 20:11:37 +0200

    js update

---
+++ routers/api.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const fsp = require('fs/promises');
+const path = require('path');
+const bp = require('body-parser')
+router.use(bp.json())
+router.use(bp.urlencoded({ extended: true }))
+const db = require('../globalurlobj')
+
+
+const generateShortUrl = ()=>{
+    let uid = 999999;
+    const shortUrl = `${uid.toString(36)}`;
+    uid+=Math.floor(Math.random()*10)
+    return shortUrl;
+}
+
+router.post('/shorturl/', function (req, res) {
+    const {originalUrl} = req.body;
+    const shortUrl = generateShortUrl();
+    db[shortUrl] = originalUrl;
+    console.log(db);
+    res.send(`http://localhost:3000/s/${shortUrl}`);
+    res.end();
+})
+
+router.get('/statistic/:shorturl-id', function (req, res) {
+    res.send('root')
+})
+
+module.exports = router;
--- src/main.js
+++ src/main.js
@@ -2,3 +2,33 @@
 import 'bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from 'axios';
+
+
+const isValidHttpUrl = (string)=> {
+    let url;
+    try {
+      url = new URL(string);
+      console.log(url);
+    } catch (_) {
+      return false;  
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
+}
+
+const enableShortBtn = ()=>{
+    document.getElementById("short-btn").disabled = false;
+}
+
+const disableShortBtn = ()=>{
+    if(!document.getElementById('url-inpt').value){
+        document.getElementById("short-btn").disabled = true;
+    }
+}
+
+
+
+
+//event listeners
+document.getElementById('url-inpt').addEventListener('input',enableShortBtn)
+document.getElementById('url-inpt').addEventListener('blur',disableShortBtn)
+document.getElementById('short-btn').addEventListener('click',ShortenUrl)
