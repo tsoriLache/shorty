10688498fdc619aac969782d2f1320696113774b
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-04 22:25:56 +0200

    update app.js

---
+++ src/back/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const cors = require("cors");
+const api = require('./routers/api')
+const redirect = require('./routers/redirect');
+const errorHandler = require('./errorHandler');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', express.static(path.resolve('./dist'))); // serve main path as static dir
+app.get('/', function(req, res) { // serve main path as static file
+  res.sendFile(path.resolve('./dist/index.html'))
+});
+
+app.use('/api/',api);
+app.use('/s/',redirect);
+app.use(errorHandler)
+
+// start the server
+app.listen(port, function() {
+  console.log('app started');
+});
