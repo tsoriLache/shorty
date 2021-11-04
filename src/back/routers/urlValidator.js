db33b4a0bd88e8b7f8b3f1556daf0ea8d9ea8f16
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-04 22:29:45 +0200

    url back validator

---
+++ src/back/routers/urlValidator.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const validator = require('validator')
+
+router.post('/shorturl', async function(req, res,next) {
+    const {originalUrl} = req.body;
+    if(validator.isURL(originalUrl)){
+        console.log('true');
+        next()
+    }else{
+        next({status:400,message:'not valid url'}) 
+    }
+})
+
+module.exports = router;
