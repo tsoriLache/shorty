3e408dec1440c6a06ba1d7c6b7f92b622e944690
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-05 14:12:35 +0200

    changed port

--- src/back/app.js
+++ src/back/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const api = require('./routers/api')
 const redirect = require('./routers/redirect');
